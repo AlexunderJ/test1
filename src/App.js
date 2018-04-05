70c4457a58b1898d7c6493dd23008e78b4b01ee7
AleksunderJ <pozdrower@wp.pl>
2018-04-05 19:49:43 +0200

    dodanie API pogody

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+
 const API = 'b2680d43ae8a0c7b35dab5ccf32e3bcd';
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=London';
+
+
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state={
+      count: 1,
+      weather: null,
+    }
+  }
+
+  componentDidMount() {
+    axios.get(url, {params: {APPID: API}}).then(resp => {
+      this.setState({weather: resp.data});
+    });
+  }
+
+  shouldComponentUpdate() {
+    return this.state.count<10;
+  }
+
+  incrementCount() {
+    const item = this.state.count + 1;
+    this.setState({count: item});
+  }
+
+  displayWeaterTemp() {
+    const { weather } = this.state;
+    return (<p>Temperatura w {weather.name} to {weather.main.temp} [in retard unit]</p>);
+  }
+  
+  render() { 
+    const { count, weather } = this.state;
+ 
     return (
       <div className="App">
         <header className="App-header">
@@ -14,6 +49,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {!!weather ? this.displayWeaterTemp() : null}
+        <div>{count}</div>
+        <button onClick={() => this.incrementCount()}> TOUCH ME </button>
       </div>
     );
   }
