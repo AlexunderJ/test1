91d5dc6b267c9aa68137d35a292a692af0c1a30f
AleksunderJ <pozdrower@wp.pl>
2018-04-05 18:31:54 +0200

    create application base

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+const API = 'b2680d43ae8a0c7b35dab5ccf32e3bcd';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
